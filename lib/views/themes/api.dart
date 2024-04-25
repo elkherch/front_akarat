eeded691dd6cb4b58ebef121f20d5fed5e45d7d2
elkherchi <22025@supnum.mr>
2024-04-25 22:21:29 +0000

    "push"

--- lib/views/themes/api.dart
+++ lib/views/themes/api.dart
@@ -1,16 +1,17 @@
 // ignore_for_file: constant_identifier_names
 
+
 class Apilink {
   static const String url = 'http://khdev.pythonanywhere.com';
-  static const list_biens = "http://10.0.2.2:8000/biens_immobiliers/";
-  static const details_biens = "http://10.0.2.2:8000/fetch_biens_immobiliers/";
-  static const user_register = "http://10.0.2.2:8000/create_user/";
-  static const user_favorie = "http://10.0.2.2:8000/liste_biens/";
-  static const liste_favorie = "http://10.0.2.2:8000/favorie/";
-  static const create_favorie = "http://10.0.2.2:8000/create_favorie/";
-  static const delete_favorie = "http://10.0.2.2:8000/delete_favorie/";
-  static const user_login = "http://10.0.2.2:8000/login/";
-  static const fethUser = "http://10.0.2.2:8000/fetch_user/";
-  static const createImmobilie = "http://10.0.2.2:8000/create_biens_immobiliers/";
-  static const recherche_bien = "http://10.0.2.2:8000/rechercher_biens/";
+  static const list_biens = "http://khdev.pythonanywhere.com/biens_immobiliers/";
+  static const details_biens = "http://khdev.pythonanywhere.com/fetch_biens_immobiliers/";
+  static const user_register = "http://khdev.pythonanywhere.com/create_user/";
+  static const user_favorie = "http://khdev.pythonanywhere.com/liste_biens/";
+  static const liste_favorie = "http://khdev.pythonanywhere.com/favorie/";
+  static const create_favorie = "http://khdev.pythonanywhere.com/create_favorie/";
+  static const delete_favorie = "http://khdev.pythonanywhere.com/delete_favorie/";
+  static const user_login = "http://khdev.pythonanywhere.com/login/";
+  static const fethUser = "http://khdev.pythonanywhere.com/fetch_user/";
+  static const createImmobilie = "http://khdev.pythonanywhere.com/create_biens_immobiliers/";
+  static const recherche_bien = "http://khdev.pythonanywhere.com/rechercher_biens/";
 }
